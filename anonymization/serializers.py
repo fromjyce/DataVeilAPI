f71d00ecddddb15861fcd8e323427e63bd134f2f
Jayashre <jaya2004kra@gmail.com>
2024-09-01 00:10:24 +0530

    added techniques

--- anonymization/serializers.py
+++ anonymization/serializers.py
@@ -5,3 +5,11 @@ class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Data
         fields = '__all__'
+
+class AnonymizationSerializer(serializers.Serializer):
+    data = serializers.CharField()
+    technique = serializers.ChoiceField(choices=[
+        'masking', 'generalization', 'k-anonymity', 'randomization',
+        'aggregation', 'perturbation', 'pseudonymization', 'data_swapping',
+        'synthetic_data'
+    ])
