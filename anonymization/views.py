a3b69a9b6fd5d02fc961f858c56b7c33dc97ad3d
Jayashre <jaya2004kra@gmail.com>
2024-09-01 00:12:18 +0530

    added views

--- anonymization/views.py
+++ anonymization/views.py
@@ -1,11 +1,15 @@
 from rest_framework import views, status
 from rest_framework.response import Response
-from .serializers import DataSerializer
-from .anonymization_utils import anonymize_data
+from .serializers import AnonymizationSerializer
+from .algorithms import anonymize_data
 
 class AnonymizeDataView(views.APIView):
     def post(self, request):
-        content = request.data.get('content')
-        technique = request.data.get('technique')
-        anonymized_data = anonymize_data(content, technique)
-        return Response({'anonymized_content': anonymized_data}, status=status.HTTP_200_OK)
+        serializer = AnonymizationSerializer(data=request.data)
+        if serializer.is_valid():
+            content = serializer.validated_data.get('data')
+            technique = serializer.validated_data.get('technique')
+            anonymized_data = anonymize_data(content, technique)
+            return Response({'anonymized_data': anonymized_data}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
