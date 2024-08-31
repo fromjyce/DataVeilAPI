f7f3f5373f3a92178a94df09088b56ad681d6730
Jayashre <jaya2004kra@gmail.com>
2024-09-01 00:13:17 +0530

    added urls

--- dataveilapi/urls.py
+++ dataveilapi/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from .views import AnonymizeDataView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('anonymize/', AnonymizeDataView.as_view(), name='anonymize_data'),
+    path('admin/', admin.site.urls),
+    path('api/', include('anonymization.urls')),
 ]
