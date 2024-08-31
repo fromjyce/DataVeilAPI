fd2d7e05ecdc7f5ab8ec5e244f7d81868aba08b6
Jayashre <jaya2004kra@gmail.com>
2024-08-31 23:58:12 +0530

    Added views

--- anonymization/views.py
+++ anonymization/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.response import Response
+from .serializers import DataSerializer
+from .anonymization_utils import anonymize_data
 
-# Create your views here.
+class AnonymizeDataView(views.APIView):
+    def post(self, request):
+        content = request.data.get('content')
+        technique = request.data.get('technique')
+        anonymized_data = anonymize_data(content, technique)
+        return Response({'anonymized_content': anonymized_data}, status=status.HTTP_200_OK)
